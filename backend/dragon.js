4a1f75c4a815469f624564a5ba583c5567c59dec
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-04 18:59:32 -0400

    Updated to add traits to the dragon class

--- backend/dragon.js
+++ backend/dragon.js
@@ -1,14 +1,29 @@
+const TRAITS = require('./traits')
+
 const DEFAULT_PROPERTIES = {
     nickname: 'unnamed',
     get birthdate() {
         return new Date();
+    },
+    get randomTraits() {
+        const traits = [];
+        TRAITS.forEach(TRAIT => {
+            const traitType = TRAIT.type;
+            const traitValues = TRAIT.values;
+            const traitValue = traitValues[
+                Math.floor(Math.random() * traitValues.length)
+            ];
+            traits.push({traitType, traitValue});
+        });
+        return traits;
     }
 }
 
 class Dragon {
-    constructor({birthdate, nickname} = {}) {//The ={} after the props declaration is so the default values can be used when not specified
+    constructor({birthdate, nickname, traits} = {}) {//The ={} after the props declaration is so the default values can be used when not specified
         this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate;
         this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
+        this.traits = traits || DEFAULT_PROPERTIES.randomTraits;
     }
 }
 
