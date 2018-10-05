e446b1c5c345b72dcd0ef9d07c996a30c0ea0708
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-05 16:00:39 -0400

    Updated to use a get request to receive a new dragon and listen on port 3000

--- backend/index.js
+++ backend/index.js
@@ -48,7 +48,14 @@
 const express = require('express');
 const GenerationEngine = require('./engine.js');
 
+const app = express();
 const engine = new GenerationEngine();
+const port = 3000
 
 engine.start();
 
+app.get('/dragon/new', (req, res) => {
+    res.json({ dragon: engine.generation.newDragon() });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
