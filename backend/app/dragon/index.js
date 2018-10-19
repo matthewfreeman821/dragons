ac330389b49efa23de1058918d68e00d2a4eb8a5
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-18 20:28:07 -0400

    Updated dragon to include dragonId, added functionality to retrieve a dragon with all dragonTraits in helper.js file

--- backend/app/dragon/index.js
+++ backend/app/dragon/index.js
@@ -1,6 +1,7 @@
 const TRAITS = require('../../data/traits')
 
 const DEFAULT_PROPERTIES = {
+    dragonId: undefined,
     nickname: 'unnamed',
     generationId: undefined,
     get birthdate() {
@@ -21,7 +22,8 @@ const DEFAULT_PROPERTIES = {
 }
 
 class Dragon {
-    constructor({birthdate, nickname, traits, generationId} = {}) {//The ={} after the props declaration is so the default values can be used when not specified
+    constructor({ dragonId, birthdate, nickname, traits, generationId } = {}) {//The ={} after the props declaration is so the default values can be used when not specified
+        this.dragonId = dragonId || DEFAULT_PROPERTIES.dragonId;
         this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate;
         this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
         this.traits = traits || DEFAULT_PROPERTIES.randomTraits;
