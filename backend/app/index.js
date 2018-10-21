dc708f0cb30e6bb563731680c92d944ef40bedec
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-21 15:45:17 -0400

    Updated to include cors in backend directory

--- backend/app/index.js
+++ backend/app/index.js
@@ -46,6 +46,7 @@
 //This was setup when the Generation Engine was created in order to test
 ///////////////////////////////////////////////////////////////////////////////////
 const express = require('express');
+const cors = require('cors');
 const GenerationEngine = require('./generation/engine');
 const dragonRouter = require('./api/dragon');
 const generationRouter = require('./api/generation');
@@ -54,6 +55,8 @@ const app = express();
 const engine = new GenerationEngine();
 
 app.locals.engine = engine;
+
+app.use(cors({ origin: 'http://localhost:1234'}));
 
 app.use('/dragon', dragonRouter);
 app.use('/generation', generationRouter);
