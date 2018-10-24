90467594cf7d5e402474c8c7fbbe918d56bbaa54
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-24 16:35:01 -0400

    Updated filepath name in Dragon require

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Generation from './components/Generation';
-import Dragon from './components/dragon';
+import Dragon from './components/Dragon';
 
 render(
     <div>
