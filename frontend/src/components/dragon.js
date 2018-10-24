44465e0f4fd94a9b4d83100399fa63783194aad1
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-24 15:50:28 -0400

    Updated to include DragonAvatar component and call to the component in the Dragon component

--- frontend/src/components/dragon.js
+++ frontend/src/components/dragon.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import DragonAvatar from './DragonAvatar';
 
 const DEFAULT_DRAGON = {
     dragonId: '',
@@ -23,15 +24,7 @@ class Dragon extends Component {
     }
 
     render() {
-        const { generationId, dragonId, traits } = this.state.dragon;
-
-        return (
-            <div>
-                <span>G{generationId}.</span>
-                <span>I{dragonId}. </span>
-                { traits.map(trait => trait.traitValue).join(', ') }
-            </div>
-        )
+        return <DragonAvatar />;
     }
 }
 
