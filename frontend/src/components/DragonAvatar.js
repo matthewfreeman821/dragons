83d950464f99e492d180bf244f875fbcbf55db01
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-26 14:50:00 -0400

    Added function into render

--- frontend/src/components/DragonAvatar.js
+++ frontend/src/components/DragonAvatar.js
@@ -32,6 +32,7 @@ class DragonAvatar extends Component {
                 <span>G{generationId}.</span>
                 <span>I{dragonId}. </span>
                 { traits.map(trait => trait.traitValue).join(', ') }
+                { this.DragonImage }
             </div>
         )
     }
