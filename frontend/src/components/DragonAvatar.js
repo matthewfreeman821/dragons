b85f94e64bf1a971d86632932d3b0d33c3f9a8a9
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-26 14:18:40 -0400

    Added propertyMap to dragonAvatar, initial setup for displaying a dragon based on traits

--- frontend/src/components/DragonAvatar.js
+++ frontend/src/components/DragonAvatar.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
 
+const propertyMap = {
+    backgroundColor: { 
+        black: '#000000', 
+        white: '#FFFFFF',
+        green: '#00FF00', 
+        blue: '#0000FF' 
+    },
+    build: { slender, stocky, sporty, skinny },
+    pattern: { plain, striped, spotted, patchy},
+    size: { small, medium, large, enormous }
+};
+
 class DragonAvatar extends Component {
     render() {
         const { generationId, dragonId, traits } = this.props.dragon;
