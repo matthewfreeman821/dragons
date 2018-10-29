4a612178ce2d1efdaa6a6fbcec6766c931c0e157
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-29 12:34:27 -0400

    Updated structure to include directories for actions and reducers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,18 +5,6 @@ import Generation from './components/Generation';
 import Dragon from './components/Dragon';
 import './index.css';
 
-const DEFAULT_GENERATION = { generationId: '', expiration: '' }
-
-const GENERATION_ACTION_TYPE = 'GENERATION_ACTION_TYPE'
-
-const generationReducer = (state, action) => {
-    if (action.type === GENERATION_ACTION_TYPE) {
-        return { generation: action.generation };
-    }
-
-    return { generation: DEFAULT_GENERATION };
-}
-
 const store = createStore(generationReducer);
 
 store.subscribe(() => console.log('store state update', store.getState()));
@@ -30,13 +18,6 @@ store.dispatch({
 });
 
 console.log('store.getState()', store.getState());
-
-const generationActionCreator = (payload) => {
-    return {
-        type: GENERATION_ACTION_TYPE,
-        generation: payload
-    };
-}
 
 const zooAction = generationActionCreator({
     generationId: 'zoo', expiration: 'bar'
