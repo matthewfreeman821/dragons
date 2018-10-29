7a3797f0347d1cd02ae9e0d2560d841a9bb1b616
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-29 12:39:38 -0400

    Refactored index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,27 +3,13 @@ import { createStore } from 'redux';
 import { render } from 'react-dom';
 import Generation from './components/Generation';
 import Dragon from './components/Dragon';
+import { generationReducer } from './reducers';
+import { generationActionCreator } from './actions/generation';
 import './index.css';
 
 const store = createStore(generationReducer);
 
 store.subscribe(() => console.log('store state update', store.getState()));
-
-console.log('store', store);
-
-store.dispatch({ type: 'foo' });
-store.dispatch({ 
-    type: GENERATION_ACTION_TYPE,
-    generation: { generationId: 'goo', expiration: 'bar'}
-});
-
-console.log('store.getState()', store.getState());
-
-const zooAction = generationActionCreator({
-    generationId: 'zoo', expiration: 'bar'
-});
-
-store.dispatch(zooAction);
 
 fetch('http://localhost:3000/generation')
     .then(response => response.json())
