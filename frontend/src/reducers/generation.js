8a4dccb55dc4841f871624b75ef7330e87792658
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-30 11:51:26 -0400

    Updated to have a fetch status in reducer

--- frontend/src/reducers/generation.js
+++ frontend/src/reducers/generation.js
@@ -1,16 +1,17 @@
 import { GENERATION } from '../actions/types';
+import fetchStates from './fetchStates';
 
 const DEFAULT_GENERATION = { generationId: '', expiration: '' }
 
-
+//This is used to so my reducer fucntion can handle the three different states the fetch could be in
 const generationReducer = (state = DEFAULT_GENERATION, action) => {
     switch(action.type) {
         case GENERATION.FETCH:
-            return state;
+            return { ...state, status: fetchStates.fetching };
         case GENERATION.FETCH_ERROR:
-            return { ...state, message: action.message };
+            return { ...state, status: fetchStates.error, message: action.message };
         case GENERATION.FETCH_SUCCESS:
-            return { ...state, ...action.generation };
+            return { ...state, status: fetchStates.success, ...action.generation };
         default:
             return state;
     }
