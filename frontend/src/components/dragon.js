e513211af025b9357ad9fc6ff1805c1970d49b7d
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-30 14:02:26 -0400

    Updated to remove all unneccesary lines using state because of the switch to props

--- frontend/src/components/dragon.js
+++ frontend/src/components/dragon.js
@@ -4,33 +4,12 @@ import { Button } from 'react-bootstrap';
 import DragonAvatar from './DragonAvatar';
 import { fetchDragon } from '../actions/dragon';
 
-const DEFAULT_DRAGON = {
-    dragonId: '',
-    generationId: '',
-    nickname: '',
-    birthdate: '',
-    traits: []
-};
-
 class Dragon extends Component {
-    state = { dragon: DEFAULT_DRAGON };
-
-    componentDidMount() {
-        this.fetchDragon();
-    }
-
-    fetchDragon = () => {
-        fetch('http://localhost:3000/dragon/new')
-            .then(response => response.json())
-            .then(json => this.setState({ dragon: json.dragon }))
-            .catch(error => console.error('error', error));
-    }
-
     render() {
         return (
             <div>
-                <Button onClick={() => this.fetchDragon()}>New Dragon</Button>
-                <DragonAvatar dragon={this.state.dragon} />
+                <Button onClick={this.props.fetchDragon}>New Dragon</Button>
+                <DragonAvatar dragon={this.props.dragon} />
             </div>
         );
     }
