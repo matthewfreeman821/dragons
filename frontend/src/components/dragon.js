47b777895f74c080ac6631144129045dd87cbadd
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-31 11:40:42 -0400

    Updated styling, again

--- frontend/src/components/dragon.js
+++ frontend/src/components/dragon.js
@@ -8,9 +8,11 @@ class Dragon extends Component {
     render() {
         return (
             <div className="container">
-                <div className="col-lg-6">
-                    <DragonAvatar dragon={this.props.dragon} />
-                    <Button className="btn-primary"onClick={this.props.fetchDragon}>New Dragon</Button>
+                <div className="row">
+                    <div className="col-lg-6">
+                        <DragonAvatar dragon={this.props.dragon} />
+                        <Button className="btn-primary"onClick={this.props.fetchDragon}>New Dragon</Button>
+                    </div>
                 </div>
             </div>
         );
