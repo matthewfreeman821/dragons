057f4ef30dddd055177cbf321c69b8c2f36c8926
Matthew Freeman <matthew.freeman821@gmail.com>
2018-11-09 17:07:00 -0500

    Updated to include fetchFromAccount function which is then utilized in the signup and logout methods

--- frontend/src/actions/account.js
+++ frontend/src/actions/account.js
@@ -1,25 +1,39 @@
 import { ACCOUNT } from './types';
 import { BACKEND } from '../config';
 
-export const signup = ({ username, password }) => dispatch => {
+const fetchFromAccount = ({ endpoint, options, SUCCESS_TYPE }) => dispatch => {
     dispatch({ type: ACCOUNT.FETCH });
 
-    return fetch(`${BACKEND.ADDRESS}/account/signup`, {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json'},
-        credentials: 'include'
-    })
+    return fetch(`${BACKEND.ADDRESS}/account/${endpoint}`, options)
         .then(response => response.json())
         .then(json => {
             if (json.type === 'error') {
                 dispatch({ type: ACCOUNT.FETCH_ERROR, message: json.message });
             } else {
-                dispatch({ type: ACCOUNT.FETCH_SUCCESS, ...json });
+                dispatch({ type: SUCCESS_TYPE, ...json });
             }
         })
         .catch(error => dispatch({ 
             type: ACCOUNT.FETCH_ERROR, 
             message: error.message 
         }));
-};
+}
+
+export const signup = ({ username, password }) => fetchFromAccount({
+    endpoint: 'signup',
+    options: {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json'},
+        credentials: 'include'
+    },
+    SUCCESS_TYPE: ACCOUNT.FETCH_SUCCESS
+});
+
+export const logout = () => fetchFromAccount({
+    endpoint: 'logout',
+    options: {
+        credentials: 'include',
+    },
+    SUCCESS_TYPE: ACCOUNT.FETCH_LOGOUT_SUCCESS
+});
