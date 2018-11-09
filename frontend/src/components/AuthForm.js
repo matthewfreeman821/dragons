d0e62dffd9dd5580743128611a5863f9a9071be2
Matthew Freeman <matthew.freeman821@gmail.com>
2018-11-09 16:03:11 -0500

    Updated to include signup function on auth form page

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Button, FormGroup, FormControl } from 'react-bootstrap';
+import { signup } from '../actions/account';
 
 class AuthForm extends Component {
     state = { username: '', password: '' };
@@ -14,7 +16,9 @@ class AuthForm extends Component {
     }
 
     signup = () => {
-        console.log('this.state', this.state);
+        const { username, password } = this.state;
+
+        this.props.signup({ username, password });
     }
 
     login = () => {
@@ -51,4 +55,4 @@ class AuthForm extends Component {
     }
 }
 
-export default AuthForm;
+export default connect(null, { signup })(AuthForm);
