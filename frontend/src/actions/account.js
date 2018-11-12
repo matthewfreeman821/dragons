78113c80d1ed49e1c44b68702a603a352cfc481d
Matthew Freeman <matthew.freeman821@gmail.com>
2018-11-12 13:46:46 -0500

    Refactored fetchFromAccount method to more generic for reducer types

--- frontend/src/actions/account.js
+++ frontend/src/actions/account.js
@@ -1,20 +1,26 @@
 import { ACCOUNT } from './types';
 import { BACKEND } from '../config';
 
-export const fetchFromAccount = ({ endpoint, options, SUCCESS_TYPE }) => dispatch => {
+export const fetchFromAccount = ({ 
+    endpoint, 
+    options, 
+    FETCH_TYPE,
+    ERROR_TYPE,
+    SUCCESS_TYPE 
+}) => dispatch => {
     dispatch({ type: ACCOUNT.FETCH });
 
     return fetch(`${BACKEND.ADDRESS}/account/${endpoint}`, options)
         .then(response => response.json())
         .then(json => {
             if (json.type === 'error') {
-                dispatch({ type: ACCOUNT.FETCH_ERROR, message: json.message });
+                dispatch({ type: ERROR_TYPE, message: json.message });
             } else {
                 dispatch({ type: SUCCESS_TYPE, ...json });
             }
         })
         .catch(error => dispatch({ 
-            type: ACCOUNT.FETCH_ERROR, 
+            type: ERROR_TYPE, 
             message: error.message 
         }));
 }
