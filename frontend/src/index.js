9244bb08d6a7085ee855f397f6aa1b324059e5e5
Matthew Freeman <matthew.freeman821@gmail.com>
2018-11-15 15:54:00 -0500

    Updated to include history in Router tag

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,14 @@ import { Provider } from 'react-redux';
 import { Router, Switch, Route } from 'react-router-dom';
 import { render } from 'react-dom';
 import thunk from 'redux-thunk';
+import createBrowserHistory from 'history/createBrowserHistory';
 import rootReducer from './reducers';
 import Root from './components/Root';
 import AccountDragons from './components/AccountDragons';
 import { fetchAuthenticated } from './actions/account';
 import './index.css';
+
+const history = createBrowserHistory();
 
 const store = createStore(
     rootReducer,
@@ -20,10 +23,10 @@ store.dispatch(fetchAuthenticated())
     .then(() => {
         render(
             <Provider store={store}>
-                <Router>
+                <Router history={history}>
                     <Switch>
                         <Route path='/' component={Root} />
-                        <Route path='/account-dragons' component={AccoutDragons} />
+                        <Route path='/account-dragons' component={AccountDragons} />
                     </Switch>
                 </Router>
             </Provider>,
