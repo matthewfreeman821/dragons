106af274a5d3c89e3194544df3ebdd94fa4b9814
Matthew Freeman <matthew.freeman821@gmail.com>
2018-11-15 15:46:54 -0500

    Updated to utilize Switch

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { Router, Switch, Route } from 'react-router-dom';
 import { render } from 'react-dom';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import Root from './components/Root';
+import AccountDragons from './components/AccountDragons';
 import { fetchAuthenticated } from './actions/account';
 import './index.css';
 
@@ -18,7 +20,12 @@ store.dispatch(fetchAuthenticated())
     .then(() => {
         render(
             <Provider store={store}>
-                <Root />
+                <Router>
+                    <Switch>
+                        <Route path='/' component={Root} />
+                        <Route path='/account-dragons' component={AccoutDragons} />
+                    </Switch>
+                </Router>
             </Provider>,
             document.getElementById('root')
         );
