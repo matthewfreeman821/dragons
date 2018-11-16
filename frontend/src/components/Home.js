e147526bd07da06bdc0ed8dc00c174904c5e506c
Matthew Freeman <matthew.freeman821@gmail.com>
2018-11-16 11:36:18 -0500

    Updated to have link to account dragons on home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
 import Generation from './Generation';
 import Dragon from './Dragon';
 import AccountDragons from './AccountDragons';
@@ -10,10 +11,14 @@ class Home extends Component {
     render() {
         return (
             <div className="row">
-                <Button onClick={this.props.logout} className='logout-button' >Log Out</Button>
+                <Button onClick={this.props.logout} className='logout-button' >
+                Log Out
+                </Button>
                 <h2 className="col-md-3">Dragon Stack</h2>
                 <Generation />
                 <Dragon />
+                <hr />
+                <Link to='/account-dragons'>Account Dragons</Link>
             </div>
         );
     }
