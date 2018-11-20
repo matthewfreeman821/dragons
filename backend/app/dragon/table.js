9431166d1328bb83828546ebcdf8e40950598f69
Matthew Freeman <matthew.freeman821@gmail.com>
2018-11-20 14:04:10 -0500

    Updated to include changes in functions for new columns in dragon table

--- backend/app/dragon/table.js
+++ backend/app/dragon/table.js
@@ -3,13 +3,13 @@ const DragonTraitTable = require('../dragonTrait/table');
 
 class DragonTable {
     static storeDragon(dragon) {
-        const { birthdate, nickname, generationId } = dragon;
+        const { birthdate, nickname, generationId, isPublic, saleValue } = dragon;
 
         return new Promise((resolve, reject) => {
             pool.query(
-                `INSERT INTO dragon(birthdate, nickname, "generationId") 
-                VALUES ($1, $2, $3) RETURNING id`,
-                [birthdate, nickname, generationId],
+                `INSERT INTO dragon(birthdate, nickname, "generationId", "isPublic", "saleValue") 
+                VALUES ($1, $2, $3, $4, $5) RETURNING id`,
+                [birthdate, nickname, generationId, isPublic, saleValue],
                 (error, response) => {
                     if (error) return reject(error);
 
@@ -32,7 +32,7 @@ class DragonTable {
     static getDragon({ dragonId }) {
         return new Promise((resolve, reject) => {
             pool.query(
-                `SELECT birthdate, nickname, "generationId" 
+                `SELECT birthdate, nickname, "generationId", "isPublic", "saleValue" 
                 FROM dragon 
                 WHERE dragon.id = $1`,
                 [dragonId],
@@ -47,11 +47,11 @@ class DragonTable {
         });
     }
 
-    static updateDragon({ dragonId, nickname }) {
+    static updateDragon({ dragonId, nickname, isPublic, saleValue }) {
         return new Promise((resolve, reject) => {
             pool.query(
-                `UPDATE dragon SET nickname = $1 WHERE id = $2`,
-                [nickname, dragonId],
+                `UPDATE dragon SET nickname = $1, "isPublic" = $2, "saleValue" = $3 WHERE id = $4`,
+                [nickname, isPublic, saleValue, dragonId],
                 (error, response) => {
                     if (error) return reject(error);
 
