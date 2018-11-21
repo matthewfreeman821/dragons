79ec8ca80a6a52276488acf82dac26506ac1fdf7
Matthew Freeman <matthew.freeman821@gmail.com>
2018-11-21 14:41:25 -0500

    Updated to include new method to update the dragon account

--- backend/app/accountDragon/table.js
+++ backend/app/accountDragon/table.js
@@ -42,7 +42,20 @@ class AccountDragonTable {
             )
         });
     };
-    
+
+    static updateDragonAccount({ dragonId, accountId }) {
+        return new Promise((resolve, reject) => {
+            pool.query(
+                `UPDATE accountDragon SET "accountId" = $1 WHERE "dragonId" = $2`,
+                [accountId, dragonId],
+                (error, response) => {
+                    if (error) return reject(error);
+
+                    resolve();
+                }
+            )
+        });
+    };
 }
 
 // Below is debugging code to check that the functions above work properly
