b09e84cd8219f976bca5cea4a0f150a68fec72f7
Matthew Freeman <matthew.freeman821@gmail.com>
2018-11-21 15:41:43 -0500

    Updated to include buy button and function in public dragon rows component

--- frontend/src/components/PublicDragonRow.js
+++ frontend/src/components/PublicDragonRow.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import DragonAvatar from './DragonAvatar';
+import {BACKEND } from '../config';
 
 class PublicDragonRow extends Component {
+    buy = () => {
+        const { dragonId, saleValue } = this.props.dragon;
+
+        fetch(`${BACKEND.ADDRESS}/dragon/buy`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ dragonId, saleValue })
+        })
+        .then(response => response.json())
+        .then(json => {
+            alert(json.message);
+        })
+        .catch(error => alert(error.message));
+    };
+
     render() {
         return (
             <div>
@@ -12,6 +30,8 @@ class PublicDragonRow extends Component {
                 <div>
                     Sale Value: {this.props.dragon.saleValue}
                 </div>
+                <br/>
+                <Button onClick={this.buy}>Buy</Button>
             </div>
         )
     }
