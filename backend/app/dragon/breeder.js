c3bdb2289b07035091316f25f2ce6edf164f3a2e
Matthew Freeman <matthew.freeman821@gmail.com>
2018-11-27 12:22:56 -0500

    Updated to include a Breeder class in preparation for breeding dragons

---
+++ backend/app/dragon/breeder.js
@@ -0,0 +1,27 @@
+const Dragon = require('./index');
+
+class Breeder {
+    static breedDragon({ matron, patron }) {
+        const matronTraits = matron.traits;
+        const patronTraits = patron.traits;
+
+        const babyTraits = [];
+
+        matronTraits.forEach(({ traitType, traitValue }) => {
+            const matronTrait = traitValue;
+
+            const patronTrait = patronTraits.find(
+                trait => trait.traitType === traitType
+            );
+
+            babyTraits.push({
+                traitType,
+                traitValue: Breeder.pickTrait({ matronTrait, patronTrait })
+            });
+        });
+
+        return new this.breedDragon({ nickname: 'Unnamed baby', traits: babyTraits });
+    }
+}
+
+module.exports = Breeder;
