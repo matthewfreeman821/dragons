935d429e7e07324bfa95d07d6dde587bb31a3889
Matthew Freeman <matthew.freeman821@gmail.com>
2018-11-27 14:07:45 -0500

    Updated to include sireValue in functions

--- backend/app/dragon/table.js
+++ backend/app/dragon/table.js
@@ -3,13 +3,13 @@ const DragonTraitTable = require('../dragonTrait/table');
 
 class DragonTable {
     static storeDragon(dragon) {
-        const { birthdate, nickname, generationId, isPublic, saleValue } = dragon;
+        const { birthdate, nickname, generationId, isPublic, saleValue, sireValue } = dragon;
 
         return new Promise((resolve, reject) => {
             pool.query(
-                `INSERT INTO dragon(birthdate, nickname, "generationId", "isPublic", "saleValue") 
-                VALUES ($1, $2, $3, $4, $5) RETURNING id`,
-                [birthdate, nickname, generationId, isPublic, saleValue],
+                `INSERT INTO dragon(birthdate, nickname, "generationId", "isPublic", "saleValue", "sireValue") 
+                VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`,
+                [birthdate, nickname, generationId, isPublic, saleValue, sireValue],
                 (error, response) => {
                     if (error) return reject(error);
 
@@ -32,7 +32,7 @@ class DragonTable {
     static getDragon({ dragonId }) {
         return new Promise((resolve, reject) => {
             pool.query(
-                `SELECT birthdate, nickname, "generationId", "isPublic", "saleValue" 
+                `SELECT birthdate, nickname, "generationId", "isPublic", "saleValue", "sireValue" 
                 FROM dragon 
                 WHERE dragon.id = $1`,
                 [dragonId],
@@ -47,8 +47,8 @@ class DragonTable {
         });
     }
 
-    static updateDragon({ dragonId, nickname, isPublic, saleValue }) {
-        const settingsMap = { nickname, isPublic, saleValue };
+    static updateDragon({ dragonId, nickname, isPublic, saleValue, sireValue }) {
+        const settingsMap = { nickname, isPublic, saleValue, sireValue };
 
         const validQueries = Object.entries(settingsMap).filter(([settingKey, settingValue]) => {
             if (settingValue !== undefined) {
