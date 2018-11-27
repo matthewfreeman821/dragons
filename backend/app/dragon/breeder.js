ec9999bbca1e60c34397befe7a80dc42748f74b5
Matthew Freeman <matthew.freeman821@gmail.com>
2018-11-27 13:33:22 -0500

    Imported base-64, added charSum function

--- backend/app/dragon/breeder.js
+++ backend/app/dragon/breeder.js
@@ -1,3 +1,4 @@
+const base64 = require('base-64');
 const Dragon = require('./index');
 
 class Breeder {
@@ -12,7 +13,7 @@ class Breeder {
 
             const patronTrait = patronTraits.find(
                 trait => trait.traitType === traitType
-            );
+            ).traitValue;
 
             babyTraits.push({
                 traitType,
@@ -33,12 +34,19 @@ class Breeder {
     static pickTrait({ matronTrait, patronTrait }) {
         if (matronTrait === patronTrait) return matronTrait;
 
-        const matronTraitCharSum = Breeder.charSum(matronTrait);
-        const patronTraitCharSum = Breeder.charSum(patronTrait);
+        const matronTraitCharSum = Breeder.charSum(base64.encode(matronTrait));
+        const patronTraitCharSum = Breeder.charSum(base64.encode(patronTrait));
 
         const randNum = Math.floor(Math.random() * (matronTraitCharSum + patronTraitCharSum));
 
         return randNum < matronTraitCharSum ? matronTrait : patronTrait;
+    }
+
+    static charSum(string) {
+        return string.split('').reduce(
+            (sum, character) => sum += character.charCodeAt(),
+            0
+        );
     }
 }
 
