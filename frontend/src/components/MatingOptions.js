b4526ea2cfe28b7a2e183a757a4c584fcf7233c5
Matthew Freeman <matthew.freeman821@gmail.com>
2018-11-28 14:16:14 -0500

    Updated to include mate method when button is clicked

--- frontend/src/components/MatingOptions.js
+++ frontend/src/components/MatingOptions.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
+import { BACKEND } from '../config';
+import history from '../history';
 
 class MatingOptions extends Component {
+    mate = ({ matronDragonId, patronDragonId }) => {
+        fetch(`${BACKEND.ADDRESS}/dragon/mate`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ matronDragonId, patronDragonId })
+        })
+        .then(response => response.json())
+        .then(json => {
+            alert(json.message);
+
+            if (json.type !== 'error') {
+                history.push('/account-dragons');
+            }
+        })
+        .catch(error => alert(error.message));
+    }
+
     render() {
         return(
             <div>
@@ -15,7 +35,12 @@ class MatingOptions extends Component {
 
                         return(
                             <span key={dragonId}>
-                                <Button>
+                                <Button onClick={
+                                    this.mate({
+                                        patronDragonId: this.props.patronDragonId,
+                                        matronDragonId: dragon.dragonId
+                                    })
+                                }>
                                     G{generationId}.I{dragonId}. {nickname}
                                 </Button>
                                 {' '}
