f1326ef7bc10d17dc872c6fec5d1778f4b0a2620
Matthew Freeman <matthew.freeman821@gmail.com>
2018-11-28 14:34:32 -0500

    Updated to show error if user already has dragon from the generation

--- frontend/src/components/dragon.js
+++ frontend/src/components/dragon.js
@@ -3,18 +3,23 @@ import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
 import DragonAvatar from './DragonAvatar';
 import { fetchDragon } from '../actions/dragon';
+import fetchStates from '../reducers/fetchStates';
 
 class Dragon extends Component {
+    get DragonView() {
+        const { dragon } = this.props;
+
+        if (dragon.status === fetchStates.error) return <span>{dragon.message}</span>;
+        
+        return <DragonAvatar dragon={this.props.dragon} />;
+    }
+
     render() {
         return (
-            // <div className="container">
-                //<div className="row">
-                    <div className="col-lg-3 dragon-information">
-                        <DragonAvatar dragon={this.props.dragon} />
-                        <Button className="btn-primary"onClick={this.props.fetchDragon}>New Dragon</Button>
-                    </div>
-                // </div>
-            // </div>
+            <div className="col-lg-3 dragon-information">
+                <Button className="btn-primary" onClick={this.props.fetchDragon}>New Dragon</Button>
+                { this.DragonView }
+            </div>
         );
     }
 }
